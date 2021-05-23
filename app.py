941a1b0d59bb03360c45dcd38a51904ec14f4e6a
tomcrane <tom@nanoplanet.com>
2021-05-23 17:08:40 +0100

    fix bugs, be more explicit

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def index():
     encode_error = ""
     decode_error = ""
     operation = request.form.get("operation")
+    no_padding = request.form.get("no-padding", None) is not None
 
     iiif_content = request.args.get("iiif-content")
 
@@ -32,21 +33,21 @@ def index():
     if iiif_content is not None:
         server_encoded = iiif_content
         try:
-            server_plain = decode(iiif_content, encoding)
+            server_plain = decode(iiif_content, encoding, no_padding)
         except Exception as ex:
             decode_error = repr(ex)
 
     if operation == "serverEncode":
         server_plain = request.form.get("server-plain")
         try:
-            server_encoded = encode(server_plain, encoding)
+            server_encoded = encode(server_plain, encoding, no_padding)
         except Exception as ex:
             encode_error = repr(ex)
 
     elif operation == "serverDecode":
         server_encoded = request.form.get("server-encoded")
         try:
-            server_plain = decode(server_encoded, encoding)
+            server_plain = decode(server_encoded, encoding, no_padding)
         except Exception as ex:
             decode_error = repr(ex)
 
@@ -54,7 +55,7 @@ def index():
         client_plain = request.form.get("client-plain")
         server_plain = client_plain
         try:
-            server_encoded = encode(server_plain, encoding)
+            server_encoded = encode(server_plain, encoding, no_padding)
         except Exception as ex:
             encode_error = repr(ex)
 
@@ -62,63 +63,96 @@ def index():
         server_encoded = request.form.get("server-encoded")
         client_encoded = server_encoded
         try:
-            server_plain = decode(server_encoded, encoding)
+            server_plain = decode(server_encoded, encoding, no_padding)
         except Exception as ex:
             decode_error = repr(ex)
 
-    return render_template("index.html", operation=operation, radios=radios,
+    chk_no_padding = "checked" if no_padding else ""
+
+    return render_template("index.html", operation=operation,
+                           radios=radios, chk_no_padding=chk_no_padding,
                            client_plain=client_plain, client_encoded=client_encoded,
                            server_plain=server_plain, server_encoded=server_encoded,
                            encode_error=encode_error, decode_error=decode_error)
 
 
-def encode(plain_text, method):
+def encode(plain_text, method, no_padding):
     if method == "simple":
-        return encode_normal_utf8(plain_text)
+        return encode_normal_utf8(plain_text, no_padding)
     elif method == "unicode":
         # going to do exactly the same here to demonstrate the problem
-        return encode_normal_utf8(plain_text)
+        return encode_normal_utf8(plain_text, no_padding)
     elif method == "encodeuri":
-        return encode_uri(plain_text)
+        return encode_uri(plain_text, no_padding)
     else:
         raise Exception("Unknown encoding option " + method)
 
 
-def decode(content_state, method):
+def decode(content_state, method, no_padding):
     if method == "simple":
-        return decode_normal_utf8(content_state)
+        return decode_normal_utf8(content_state, no_padding)
     elif method == "unicode":
         # going to do exactly the same here to demonstrate the problem
-        return decode_normal_utf8(content_state)
+        return decode_normal_utf8(content_state, no_padding)
     elif method == "encodeuri":
-        return decode_uri(content_state)
+        return decode_uri(content_state, no_padding)
     else:
         raise Exception("Unknown encoding option " + method)
 
 
-def encode_normal_utf8(plain_text):
+def encode_normal_utf8(plain_text, no_padding):
     binary = plain_text.encode("UTF-8")
     base64url = base64.urlsafe_b64encode(binary)  # this is bytes
-    return base64url.decode("UTF-8")
+    utf8_decoded = base64url.decode("UTF-8")
+    if no_padding:
+        utf8_decoded = remove_padding(utf8_decoded)
+    return utf8_decoded
 
 
-def decode_normal_utf8(content_state):
+def decode_normal_utf8(content_state, no_padding):
+    if no_padding:
+        content_state = restore_padding(content_state)
     binary = base64.urlsafe_b64decode(content_state)
     plain_text = binary.decode("UTF-8")
     return plain_text
 
 
-def encode_uri(plain_text):
+def encode_uri(plain_text, no_padding):
     quoted = urllib.parse.quote(plain_text, safe=',/?:@&=+$#')
-    base64url = base64.urlsafe_b64encode(quoted)  # this is bytes
-    return base64url.decode("UTF-8")
+    print(f"QUOTED: {quoted}")
+    binary = quoted.encode("UTF-8")
+    base64url = base64.urlsafe_b64encode(binary)  # this is bytes
+    utf8_decoded = base64url.decode("UTF-8")
+    if no_padding:
+        utf8_decoded = remove_padding(utf8_decoded)
+    return utf8_decoded
 
 
-def decode_uri(content_state):
+def decode_uri(content_state, no_padding):
+    if no_padding:
+        content_state = restore_padding(content_state)
     binary = base64.urlsafe_b64decode(content_state)
-    unquoted = urllib.parse.unquote(binary)
-    plain_text = unquoted.decode("UTF-8")
-    return plain_text
+    plain_text = binary.decode("UTF-8")
+    unquoted = urllib.parse.unquote(plain_text)
+    return unquoted
+
+
+# Padding
+# base64url spec says:
+# The pad character "=" is typically percent-encoded when used in an URI, but if the data length is known
+# implicitly, this can be avoided by skipping the padding.
+def remove_padding(s):
+    return s.replace("=", "")
+
+
+def restore_padding(s):
+    pad = len(s) % 4
+    padding = ""
+    if pad:
+        if pad == 1:
+            raise Exception("InvalidLengthError: Input base64url string is the wrong length to determine padding")
+        padding = "=" * (5 - pad)
+    return s + padding
 
 
 if __name__ == '__main__':
--- static/base64url.js
+++ static/base64url.js
@@ -4,24 +4,30 @@
 // and
 // https://stackoverflow.com/a/51838635
 
-function encodeSimple(plainContentState) {
+function encodeSimple(plainContentState, noPadding) {
     let base64 = btoa(plainContentState);
-    return replaceCharsToBase64url(base64);
+    let base64url = base64ToBase64url(base64);
+    if(noPadding) base64url = removePadding(base64url);
+    return base64url;
 }
 
-function decodeSimple(base64url) {
-    let base64 = replaceCharsAndPadFromBase64url(base64url);
+function decodeSimple(base64url, noPadding) {
+    if(noPadding) base64url = restorePadding(base64url);
+    let base64 = base64urlToBase64(base64url);
     return atob(base64);
 }
 
-function encodeBinary(plainContentState) {
+function encodeBinary(plainContentState, noPadding) {
     let safeString = toBinary(plainContentState);
     let base64 = btoa(safeString);
-    return replaceCharsToBase64url(base64);
+    let base64url = base64ToBase64url(base64);
+    if(noPadding) base64url = removePadding(base64url);
+    return base64url;
 }
 
-function decodeBinary(base64url) {
-    let base64 = replaceCharsAndPadFromBase64url(base64url);
+function decodeBinary(base64url, noPadding) {
+    if(noPadding) base64url = restorePadding(base64url);
+    let base64 = base64urlToBase64(base64url);
     let decoded = atob(base64);
     return fromBinary(decoded);
 }
@@ -29,27 +35,36 @@ function decodeBinary(base64url) {
 // This is deliberately encodeURI rather than encodeURIComponent.
 // This is slightly more compact if we are going to base64 anyway.
 // https://thisthat.dev/encode-uri-vs-encode-uri-component/
-function encodeUriEncode(plainContentState) {
+function encodeUriEncode(plainContentState, noPadding) {
     let uriEncoded = encodeURI(plainContentState);
+    console.log(uriEncoded)
     let base64 = btoa(uriEncoded);
-    return replaceCharsToBase64url(base64);
+    let base64url = base64ToBase64url(base64);
+    if(noPadding) base64url = removePadding(base64url);
+    return base64url;
 }
 
-function decodeUriEncode(base64url) {
-    let base64 = replaceCharsAndPadFromBase64url(base64url);
-    let decoded = btoa(base64);
+function decodeUriEncode(base64url, noPadding) {
+    if(noPadding) base64url = restorePadding(base64url);
+    let base64 = base64urlToBase64(base64url);
+    let decoded = atob(base64);
     return decodeURI(decoded);
 }
 
-
-function replaceCharsToBase64url(base64) {
-    return base64.replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
+function base64ToBase64url(base64) {
+    return base64.replace(/\+/g, "-").replace(/\//g, "_");
 }
 
-function replaceCharsAndPadFromBase64url(base64url) {
+function removePadding(s) {
+    return s.replace(/=/g, "");
+}
+
+function base64urlToBase64(base64url) {
     // Replace non-url compatible chars with base64 standard chars
-    let s = base64url.replace(/-/g, '+').replace(/_/g, '/');
-    // Pad out with standard base64 required padding characters
+    return base64url.replace(/-/g, '+').replace(/_/g, '/');
+}
+
+function restorePadding(s) {
     let pad = s.length % 4;
     if (pad) {
         if (pad === 1) {
@@ -59,6 +74,25 @@ function replaceCharsAndPadFromBase64url(base64url) {
     }
     return s;
 }
+
+
+// function replaceCharsToBase64url(base64) {
+//     return base64.replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
+// }
+
+// function replaceCharsAndPadFromBase64url(base64url) {
+//     // Replace non-url compatible chars with base64 standard chars
+//     let s = base64url.replace(/-/g, '+').replace(/_/g, '/');
+//     // Pad out with standard base64 required padding characters
+//     let pad = s.length % 4;
+//     if (pad) {
+//         if (pad === 1) {
+//             throw new Error('InvalidLengthError: Input base64url string is the wrong length to determine padding');
+//         }
+//         s += new Array(5 - pad).join('=');
+//     }
+//     return s;
+// }
 
 // convert a Unicode string to a string in which
 // each 16-bit unit occupies only one byte
